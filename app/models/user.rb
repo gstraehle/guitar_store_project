b269f7d1b30e5e6fb3cc75c8054868a7e73b0b35
Kathleen Votolato <kvotolato3@gmail.com>
2014-06-10 15:44:57 -0400

    Remove email placeholder method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,4 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :orders
-
-  def email_placeholder
-    if user_signed_in?
-      self.email
-    else
-      'Email'
-    end
-  end
 end
