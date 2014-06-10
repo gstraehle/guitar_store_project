e8632ab839a52575906eb50090631e7d88d96567
Kathleen Votolato <kvotolato3@gmail.com>
2014-06-10 15:47:06 -0400

    Remove checkout-related routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,4 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :products
-
-  get 'orders/:id/checkout', to: 'orders#checkout'
-  patch 'orders/:id/checkout', to: 'orders#convert_cart_to_order', as: 'convert'
-  get 'orders/:id/confirmation', to: 'orders#confirmation', as: 'confirmation'
-
 end
