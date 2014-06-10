1e885f7adbf87ae5aa43b9cf753dd2c73d1752b3
Amanda McDermott <amandapmcdermott@gmail.com>
2014-06-10 15:39:28 -0400

    Added set_cart method on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_cart, only: [:show, :index]
+
+  def set_cart
+    @cart = Order.where(user_id: current_user.id).find_by(processed_at: nil)
+  end
 end
