7dc276953bdf650437b4e58763deb737667417f1
George Straehle <gstraehle@gmail.com>
2014-06-11 14:47:59 -0400

    Added remove buttons to cart

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,1 +1,20 @@
-
+$(document).ready(function(){
+  $( ".remove_item" ).click(function() {
+    var confirmation = confirm('Are you sure you want to remove this item?');
+    if (confirmation == true) {
+      $.ajax({
+        url: '/order_products/' + this.id,
+        type: 'DELETE'
+      })
+      .done(function(result) {
+        console.log("success");
+      })
+      .fail(function() {
+        console.log("error");
+      })
+      .always(function() {
+        console.log("complete");
+      });
+    }
+  });
+});
