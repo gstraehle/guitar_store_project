c4efee72b07787ac07d46d79b15484b9104405c7
George Straehle <gstraehle@gmail.com>
2014-06-12 11:26:13 -0400

    Removed scaffold comments from ProductsController and a binding.pry

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,10 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
-  # GET /products
-  # GET /products.json
   def index
     @products = Product.all
   end
 
-  # GET /products/1
-  # GET /products/1.json
   def show
     if user_signed_in?
       #finds the item numbers of current user's cart
@@ -18,17 +14,13 @@ class ProductsController < ApplicationController
     end
   end
 
-  # GET /products/new
   def new
     @product = Product.new
   end
 
-  # GET /products/1/edit
   def edit
   end
 
-  # POST /products
-  # POST /products.json
   def create
     @product = Product.new(product_params)
 
@@ -44,14 +36,11 @@ class ProductsController < ApplicationController
   end
 
   def order_by_price
-    binding.pry
     @products = Product.all.sort(:price)
     @products
     redirect_to 'root_path'
   end
 
-  # PATCH/PUT /products/1
-  # PATCH/PUT /products/1.json
   def update
     respond_to do |format|
       if @product.update(product_params)
@@ -64,8 +53,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  # DELETE /products/1
-  # DELETE /products/1.json
   def destroy
     @product.destroy
     respond_to do |format|
@@ -75,12 +62,10 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params[:product]
     end
