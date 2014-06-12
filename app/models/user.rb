f1bd63ca60f229a9cb56942e9c697f6fc0c35cb5
Amanda McDermott <amandapmcdermott@gmail.com>
2014-06-12 01:04:08 -0400

    Merged with master

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,55 @@ class User < ActiveRecord::Base
     Order.create(user: self)
   end
 
+  # if user is logged in, return current_user, else return guest_user
+  def current_or_guest_user
+    if current_user
+      if session[:guest_user_id]
+        logging_in
+        guest_user.destroy
+        session[:guest_user_id] = nil
+      end
+      current_user
+    else
+      guest_user
+    end
+  end
+
+  # find guest_user object associated with the current session,
+  # creating one as needed
+  def guest_user
+    # Cache the value the first time it's gotten.
+    @cached_guest_user ||= User.find(session[:guest_user_id] ||= create_guest_user.id)
+
+  rescue ActiveRecord::RecordNotFound # if session[:guest_user_id] invalid
+     session[:guest_user_id] = nil
+     guest_user
+  end
+
+
+  private
+
+  def logging_in
+    # For example:
+    # guest_comments = guest_user.comments.all
+    # guest_comments.each do |comment|
+      # comment.user_id = current_user.id
+      # comment.save!
+    # end
+
+    guest_cart = guest_user.order.products.all
+    guest_cart.each do |product|
+
+    end
+  end
+
+  def create_guest_user
+    u = User.create(:email => "guest_#{Time.now.to_i}#{rand(99)}@example.com")
+    u.save!(:validate => false)
+    session[:guest_user_id] = u.id
+    u
+  end
+
+
+>>>>>>> Stashed changes
 end
