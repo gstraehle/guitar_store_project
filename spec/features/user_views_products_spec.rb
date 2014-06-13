2425bd2c93c1e9e4dcb6dc6e9123863116f7a97d
George Straehle <gstraehle@gmail.com>
2014-06-13 09:42:05 -0400

    refactored tests

--- spec/features/user_views_products_spec.rb
+++ spec/features/user_views_products_spec.rb
@@ -14,15 +14,15 @@ feature 'User views products' do
   end
 end
 
-
-
 feature 'User views product' do
   scenario 'on the front page' do
+
     visit root_path
+
       click_link 'Products'
-    expect(page).to have_content 'Products'
-    expect(page).to have_content 'Guitars so awesome'
-    expect(page).to have_content 'Login'
+        expect(page).to have_content 'Products'
+        expect(page).to have_content 'Guitars so awesome'
+        expect(page).to have_content 'Login'
 
   end
 end
@@ -31,9 +31,9 @@ feature 'User views product' do
   scenario 'on the front page' do
     visit root_path
       click_link 'Sign up'
-    expect(page).to have_content 'Sign up'
-    expect(page).to have_content 'City'
-    expect(page).to have_content 'Password'
+        expect(page).to have_content 'Sign up'
+        expect(page).to have_content 'City'
+        expect(page).to have_content 'Password'
 
   end
 end
@@ -42,9 +42,9 @@ feature 'User views product' do
   scenario 'on the front page' do
     visit root_path
       click_link 'Login'
-    expect(page).to have_content 'Sign in'
-    expect(page).to have_content 'Remember me'
-    expect(page).to have_content 'Password'
+        expect(page).to have_content 'Sign in'
+        expect(page).to have_content 'Remember me'
+        expect(page).to have_content 'Password'
 
   end
 end
@@ -56,8 +56,6 @@ feature 'User views product' do
         fill_in 'user_email', with: 'kag@example.com'
         fill_in 'user_password', with: 'password'
         click_button 'Sign in'
-    expect(page).to have_content 'Logged in as kag@example.com'
-
-
+          expect(page).to have_content 'Logged in as kag@example.com'
   end
 end
